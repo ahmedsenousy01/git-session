036b286438bec11a0bbd278b0863a39c86a2a08e
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2025-02-02 17:27:30 +0200

    moved the cout to a function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void printHello()
+{
+  cout << "Hello\n";
+}
+
 int main()
 {
   int n;
   cin >> n;
   for (int i = 0; i < n; i++)
-    cout << "Hello, World\n";
+    printHello();
 }
